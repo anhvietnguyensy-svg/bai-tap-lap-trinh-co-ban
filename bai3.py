b390fb2b6f7ff466f60009e01ff0c919c8507d68
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-06 11:01:40 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 n =int(input("enter a number--->"))
 if n% 2==0:
     print ("even number");
 else:
     print ("ODD Number");
+
