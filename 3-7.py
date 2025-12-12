5582edf7400fe04a04e3c04540753dd73d94e313
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:48:20 +0700

    Update 3-7.py

--- 3-7.py
+++ 3-7.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 s = input("Nhập chuỗi: ")
 kq = ""
 
@@ -6,3 +11,4 @@ for ch in s:
         kq += ch
 
 print(kq)
+
