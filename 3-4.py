2f247dc9b458ac7a2679719877e37126ee62699f
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:47:19 +0700

    Update 3-4.py

--- 3-4.py
+++ 3-4.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds=input('danh sách :').split()
 print(ds)
 
 for so in ds:
     print(so)
+
