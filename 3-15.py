b7d4f8f204d5ec991e19419610547808c19b3b4e
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:42:47 +0700

    Update 3-15.py

--- 3-15.py
+++ 3-15.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 s = input("Nhập chuỗi từ: ")
 words = s.split()
 
@@ -5,3 +10,4 @@ words.sort()
 
 for w in words:
     print(w)
+
