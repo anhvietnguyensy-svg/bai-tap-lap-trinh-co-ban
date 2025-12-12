64fa388691fc79463c18a50bda23a8e48877ae69
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:44:36 +0700

    Update 3-2.py

--- 3-2.py
+++ 3-2.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 S=input('nhap chuoi:')
 for ch in S :
     if ch == ' ' or ch =='\t':
         continue
     print(ch)
+
