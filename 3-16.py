08e1dd9d099e7781e02f8e9697419ff2daa61ced
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:43:05 +0700

    Update 3-16.py

--- 3-16.py
+++ 3-16.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 s = input("Nhập chuỗi nhị phân: ")
 lst = s.split(',')
 
 for val in lst:
     print(val)
+
