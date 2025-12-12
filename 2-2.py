7fe24eb3d7392a56bf5e09079fdbbe7667d56c64
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:37:21 +0700

    Update 2-2.py

--- 2-2.py
+++ 2-2.py
@@ -1,4 +1,10 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 def sum(a,b):
  return a+b
 c=sum(4,5);
 print("tong cua 4 va 5="+str(c))
+
