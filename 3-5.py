a6c30f2020b91d527a9dc070dcaa08fc96cc2140
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:47:34 +0700

    Update 3-5.py

--- 3-5.py
+++ 3-5.py
@@ -1,3 +1,9 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds=input('danh sach:').split()
 for item in reversed(ds):
     print(item)
+
