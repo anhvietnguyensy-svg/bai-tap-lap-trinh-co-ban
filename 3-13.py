c52fd5db98007ffc4d4aa73269c809ba00dd57d1
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:42:13 +0700

    Update 3-13.py

--- 3-13.py
+++ 3-13.py
@@ -1,3 +1,9 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 print("Vị trí của chuỗi abc là:", ds.index('abc'))
+
