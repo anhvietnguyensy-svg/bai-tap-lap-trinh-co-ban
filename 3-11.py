76bd6f3a37f7609649635ae35ce46e796d9c4e44
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:40:44 +0700

    Update 3-11.py

--- 3-11.py
+++ 3-11.py
@@ -1,6 +1,12 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.append('abc')      # thêm phần tử vào cuối list
 
 for ch in ds:
     print(ch)
+
