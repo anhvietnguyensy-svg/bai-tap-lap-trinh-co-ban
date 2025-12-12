e424866fdcc1f5ec5dc4907b9ac13ee4bc9f6e2c
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:46:50 +0700

    Update 3-3.py

--- 3-3.py
+++ 3-3.py
@@ -1,4 +1,10 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 s=input('nhap chuoi:')
 for ch in s :
     print(ch.upper())
  
+
