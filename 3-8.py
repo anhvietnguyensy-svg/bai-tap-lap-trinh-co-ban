aea1f012bdd1e93344936146ca9b158fe0097387
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:48:33 +0700

    Update 3-8.py

--- 3-8.py
+++ 3-8.py
@@ -1,3 +1,8 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds = input("Nhập dãy từ: ").split()
 
 maxlen = max(len(t) for t in ds)
@@ -5,3 +10,4 @@ maxlen = max(len(t) for t in ds)
 for t in ds:
     if len(t) == maxlen:
         print(t)
+
