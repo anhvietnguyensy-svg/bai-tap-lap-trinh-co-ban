a8a77d4215786fbd02d0f70675cb4092471f44bd
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:48:05 +0700

    Update 3-6.py

--- 3-6.py
+++ 3-6.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 hoten = input("Nhập họ tên: ").split()
 ho = hoten[0]
 ten = hoten[1]
 print("Họ:", ho)
 print("Tên:", ten)
+
