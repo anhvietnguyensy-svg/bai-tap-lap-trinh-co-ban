3adf8a4411e0d9d0b59425b8ef2a5c8a02982fb2
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:40:59 +0700

    Update 3-12.py

--- 3-12.py
+++ 3-12.py
@@ -1,6 +1,12 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.remove('123')      # xóa phần tử có giá trị '123'
 
 for ch in ds:
     print(ch)
+
