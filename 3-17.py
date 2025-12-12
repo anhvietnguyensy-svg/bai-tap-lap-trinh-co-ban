bbff27aab02def31613e54d7903edb75bb9185ba
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:43:26 +0700

    Update 3-17.py

--- 3-17.py
+++ 3-17.py
@@ -1,5 +1,11 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 n = int(input("Nhập n: "))
 
 for i in range(1, n+1):
     if n % i == 0:
         print(i)
+
