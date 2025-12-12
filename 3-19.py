c9c6947be5bf262666a96812277802e6af005143
anhvietnguyensy-svg <anhvietnguyensy@gmail.com>
2025-12-12 18:44:09 +0700

    Update 3-19.py

--- 3-19.py
+++ 3-19.py
@@ -1,2 +1,8 @@
+print("sinh vien : nguyen sy anh viet ")
+
+print("ma so sv :245751030110042")
+
+print("#############################")
 P = tuple(range(0, 1000001))
 print("Đã tạo tuple 0 → 1,000,000")
+
